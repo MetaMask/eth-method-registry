e3db0b856f9492ea52e60ba10ca7d094bd3168e4
Esteban Mino <efmino@uc.cl>
2019-03-29 15:30:28 -0300

    final format

--- index.d.ts
+++ index.d.ts
@@ -1,7 +1,7 @@
-export = MethodRegistry
 declare class MethodRegistry {
     constructor(opts: object)
 
     lookup(bytes: string): void
     parse(signature: string): void
 }
+export = MethodRegistry
