26c28d63b066408205dc8ec2814e128133ec0118
legobeat <109787230+legobeat@users.noreply.github.com>
2024-02-08 04:10:16 +0900

    chore: fix contract type (#59)
    - fix: entries function is actually a Promise
    - remove type duplication

--- types/ethjs/index.d.ts
+++ types/ethjs/index.d.ts
@@ -1,14 +1,18 @@
+declare module '@metamask/ethjs-types' {
+  export interface RegistryContract {
+    at (address: string): DeployedRegistryContract;
+  }
+  export interface DeployedRegistryContract {
+    entries (bytes: string): Promise<string[]>;
+  }
+}
 declare module '@metamask/ethjs' {
+  import type { RegistryContract } from '@metamask/ethjs-types';
+
   class Eth {
     constructor(provider: any);
 
     contract(abi: Record<string, unknown>): RegistryContract;
-  }
-  interface RegistryContract {
-    at (address: string): DeployedRegistryContract;
-  }
-  interface DeployedRegistryContract {
-    entries (bytes: string): string[];
   }
   export=Eth;
 }
